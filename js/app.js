1c032de29a1ca432000b27d0cffc78cf8d413da2
Nick Lathe <nicklathe@icloud.com>
2015-05-12 14:27:54 -0700

    Added more tests and comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-var foo = 'bar';
-
 // one way to export is like this:
 var add = exports.add = function(num1, num2) {
     return (num1 + num2);
-}
+};
 
 var Person = exports.Person = function(name, age) {
     this.name = name;
@@ -11,4 +9,12 @@ var Person = exports.Person = function(name, age) {
 };
 
 //another way is like this:
+var foo = 'bar';
+
+function arrayLength (array) {
+    return array.length;
+}
+
+// functions and variables exported here:
 module.exports.foo = foo;
+module.exports.arrayLength = arrayLength;
--- test/test.js
+++ test/test.js
@@ -9,14 +9,20 @@ describe('A basic test', function() {
 
 var foo = require('../js/app.js').foo;
 describe('foo', function() {
+    //using should
     it('should equal bar', function() {
         foo.should.equal('bar');
+    })
+    //using expect
+    it('should be a string', function() {
+        expect('foo').to.be.a('string');
     })
 })
 
 var add = require('../js/app.js').add;
 describe('add', function() {
     it('should return sum of two numbers', function() {
+        //using should
         var result = add(2,4);
         result.should.equal(6);
     })
@@ -25,8 +31,18 @@ describe('add', function() {
 var Person = require('../js/app.js').Person;
 describe('person', function() {
     it('should return a name and age', function() {
+        //using should
         var nick = new Person('Nick', 29);
         nick.name.should.equal('Nick');
         nick.age.should.equal(29);
+    })
+})
+
+var arrayLength = require('../js/app.js').arrayLength;
+describe('arrayLength', function() {
+    it('should return the length of an array', function() {
+        //using expect
+        var testArray = arrayLength([1,2,3,4]);
+        expect(testArray).to.equal(4);
     })
 })
